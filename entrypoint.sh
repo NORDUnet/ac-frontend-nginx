294fcc6ecd42cc17d7702bea47c3eff41ba28630
Markus Krogh <markus@nordu.net>
2017-07-20 12:25:35 +0200

    Adding trusted hosts for auth header

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,10 +27,20 @@ ACP_BACKENDS=$(for h in $APPSERVERS; do
                 echo "  server $h:8443;"
               done)
 
+if [ "x$AUTH_HEADER" = "x" ]; then
+  AUTH_HEADER="X-Auth-User"
+fi
+AUTH_HEADER_VAR=$(echo "$AUTH_HEADER" | tr 'A-Z-' 'a-z_')
+AUTH_TRUSTED_HOSTS=$(for ip in $TRUSTEDHOSTS; do
+                echo "\"$ip\" \$http_$AUTH_HEADER_VAR;"
+              done)
+
 sed -e "s|ACP_BACKENDS|${ACP_BACKENDS//$'\n'/\\n}|" \
-     -e "s/SP_HOSTNAME/$SP_HOSTNAME/" \
-     -e "s|SSL_CERT|$SSL_CERT|" \
-     -e "s|SSL_KEY|$SSL_KEY|" \
-     /app/nginx/ac.conf.tmpl  > /etc/nginx/conf.d/ac.conf
+    -e "s|AUTH_TRUSTED_HOSTS|${AUTH_TRUSTED_HOSTS//$'\n'/\\n}|" \
+    -e "s/SP_HOSTNAME/$SP_HOSTNAME/" \
+    -e "s|SSL_CERT|$SSL_CERT|" \
+    -e "s|SSL_KEY|$SSL_KEY|" \
+    -e "s|AUTH_HEADER|$AUTH_HEADER|" \
+    /app/nginx/ac.conf.tmpl  > /etc/nginx/conf.d/ac.conf
 
 nginx -g "daemon off;"
